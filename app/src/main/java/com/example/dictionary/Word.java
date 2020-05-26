64beec23bb6796a002fbe163bf686d175ad84946
Asamel500 <65977442+Asamel500@users.noreply.github.com>
2020-05-26 23:00:05 +0300

    add class Word

---
+++ app/src/main/java/com/example/dictionary/Word.java
@@ -0,0 +1,27 @@
+package com.example.dictionary;
+
+public class Word {
+    private String EnglishWord,RussianWord;
+    
+    public Word(String englishWord, String russianWord) {
+        EnglishWord = englishWord;
+        RussianWord = russianWord;
+    }
+    public void setEnglishWord(String englishWord) {
+        EnglishWord = englishWord;
+    }
+
+    public void setRussianWord(String russianWord) {
+        RussianWord = russianWord;
+    }
+
+
+
+    public String getEnglishWord() {
+        return EnglishWord;
+    }
+
+    public String getRussianWord() {
+        return RussianWord;
+    }
+}
