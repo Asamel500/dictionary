c6a3b5f0d2a90aa96881266f7b20762ad8deabee
Asamel500 <65977442+Asamel500@users.noreply.github.com>
2020-05-30 15:44:51 +0300

    Add ADS Interstitial

--- app/src/main/java/com/example/dictionary/MainActivity.java
+++ app/src/main/java/com/example/dictionary/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -18,6 +19,8 @@ import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     private AdView mAdView;
@@ -27,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
     private SQLLiteHelper sqlLiteHelper;
     private Switch aSwitch;
     private TextView textView;
+    private Timer timer;
+    private TimerTask timerTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,10 +50,9 @@ public class MainActivity extends AppCompatActivity {
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
         //Interstitial
-        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd = new InterstitialAd(MainActivity.this);
         mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
         mInterstitialAd.loadAd(new AdRequest.Builder().build());
-
         Button ToAddWordActivityButton = findViewById(R.id.AddWordAC);
         ToAddWordActivityButton.setOnClickListener(new ToAddWordActivityOnClickListener());
         Button ToLibraryActivityButton = findViewById(R.id.library);
@@ -62,7 +67,10 @@ public class MainActivity extends AppCompatActivity {
         aSwitch.setOnCheckedChangeListener(new SwitchListener());
         textView = findViewById(R.id.TextView);
         GenerationNewAnswer();
-    }
+        timerTask = new ADSTimerTask();
+        timer = new Timer();
+        timer.schedule(timerTask,10000);
+        }
     private class ToAddWordActivityOnClickListener implements View.OnClickListener {
 
         @Override
@@ -82,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
     private class SendOnClickListener implements View.OnClickListener{
         @Override
         public void onClick(View v) {
+
             GenerationNewAnswer();
         }
     }
@@ -97,13 +106,13 @@ public class MainActivity extends AppCompatActivity {
                 int rand = (int) (Math.random() * allWord.size());
                 iterationWordCollect.add(allWord.get(rand));
                 allWord.remove(rand);
-                if (aSwitch.isActivated())
+                if (aSwitch.isChecked())
                     wordButton[i].setText(iterationWordCollect.get(i).getEnglishWord());
                 else wordButton[i].setText(iterationWordCollect.get(i).getRussianWord());
             }
             int rand = (int) (Math.random() * iterationWordCollect.size());
             wordButton[rand].setOnClickListener(new SendOnClickListener());
-            if (aSwitch.isActivated())
+            if (aSwitch.isChecked())
                 textView.setText(iterationWordCollect.get(rand).getRussianWord());
             else textView.setText(iterationWordCollect.get(rand).getEnglishWord());
         }
@@ -113,6 +122,29 @@ public class MainActivity extends AppCompatActivity {
     private class SwitchListener implements CompoundButton.OnCheckedChangeListener {
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            GenerationNewAnswer();
+        }
+    }
+
+    private class ADSTimerTask extends TimerTask{
+
+        @Override
+        public void run() {
+            wordButton[0].setOnClickListener(new ADSOnClickListener());
+            wordButton[1].setOnClickListener(new ADSOnClickListener());
+            wordButton[2].setOnClickListener(new ADSOnClickListener());
+            wordButton[3].setOnClickListener(new ADSOnClickListener());
+                }
+    }
+
+    private class ADSOnClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            if (mInterstitialAd.isLoaded()) {
+                mInterstitialAd.show();
+            } else {
+                Log.d("TAG", "The interstitial wasn't loaded yet.");
+            }
             GenerationNewAnswer();
         }
     }
